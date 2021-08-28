3c29854af3af4110bcca926de4d4747e573c5af5
Pedro <pedropareschi15@gmail.com>
2021-08-28 18:13:17 -0300

    Field Injection

--- spring-core-annotations/src/main/java/br/com/pedropareschi/springdemo/TennisCoach.java
+++ spring-core-annotations/src/main/java/br/com/pedropareschi/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach{
 
+    @Autowired
     private FortuneService fortuneService;
 
     public TennisCoach() {
@@ -18,11 +19,11 @@ public class TennisCoach implements Coach{
 //    }
 
 
-    @Autowired
-    public void doSomeCrazyStuff(FortuneService fortuneService) {
-        System.out.println(">> inside doSomeCrazyStuff() method");
-        this.fortuneService = fortuneService;
-    }
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService fortuneService) {
+//        System.out.println(">> inside doSomeCrazyStuff() method");
+//        this.fortuneService = fortuneService;
+//    }
 
     @Override
     public String getDailyWorkout() {
