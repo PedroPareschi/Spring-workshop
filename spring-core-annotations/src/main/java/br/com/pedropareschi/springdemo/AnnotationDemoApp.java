f1dd7cb0fd78109834c51255677d96f19e8cdcd3
Pedro <pedropareschi15@gmail.com>
2021-08-28 18:59:02 -0300

    inject properties file using Java annotations

--- spring-core-annotations/src/main/java/br/com/pedropareschi/springdemo/AnnotationDemoApp.java
+++ spring-core-annotations/src/main/java/br/com/pedropareschi/springdemo/AnnotationDemoApp.java
@@ -5,11 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class AnnotationDemoApp {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Coach coach = context.getBean("tennisCoach", Coach.class);
+        TennisCoach coach = context.getBean("tennisCoach", TennisCoach.class);
 //        Coach soccerCoach = context.getBean("soccerCoach", Coach.class);
         System.out.println(coach.getDailyWorkout());
 //        System.out.println(soccerCoach.getDailyWorkout());
         System.out.println(coach.getDailyFortune());
+        System.out.println(coach.getEmail());
+        System.out.println(coach.getTeam());
         context.close();
     }
 }
--- spring-core-annotations/src/main/java/br/com/pedropareschi/springdemo/TennisCoach.java
+++ spring-core-annotations/src/main/java/br/com/pedropareschi/springdemo/TennisCoach.java
@@ -2,13 +2,20 @@ package br.com.pedropareschi.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TennisCoach implements Coach{
+public class TennisCoach implements Coach {
 
 
     private FortuneService fortuneService;
+
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
 
     public TennisCoach() {
         System.out.println(">> inside default constructor");
@@ -34,5 +41,21 @@ public class TennisCoach implements Coach{
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
     }
 }
